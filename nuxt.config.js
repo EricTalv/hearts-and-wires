d2d20477935813a891a85a05ff6e2592a0f4abe0
EricTalv <eric.talviste@tptlive.ee>
2020-12-10 23:26:49 +0200

    working on getting the font to look correctly

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,13 +10,38 @@ export default {
     title: 'heartsandwires',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
+      { hid: 'description', name: 'description', content: '' },
+      {
+        rel: "stylesheet", type: "text/css",
+        href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css",
+        integrity: "sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1",
+        crossorigin: "anonymous"
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+
+  script: [
+    {
+      src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js',
+      integrity: 'sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW',
+      crossorigin: 'anonymous'
+    },
+    {
+      src: 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js',
+      integrity: 'sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU',
+      crossorigin: 'anonymous'
+    },
+    {
+      src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js',
+      integrity: 'sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj',
+      crossorigin: 'anonymous'
+    },
+
+  ],
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
@@ -38,8 +63,13 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
-    '@nuxt/content'
+    '@nuxt/content',
   ],
+
+  bootstrapVue: {
+    bootstrapCSS: false,
+    bootstrapVueCSS: false
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
-    <h1>Welcome</h1>
+    <div class="row justify-content-center" style="display:flex; justify-content: center">
+      <h1 class="hw-title">HEARTS&WIRES</h1>
+    </div>
   </div>
 </template>
 
@@ -15,6 +17,17 @@ export default {
 </script>
 
 <style>
+  .hw-title {
+    font-size: 20rem;
 
+    color: rgba(0,0,0,0.9);
+    text-shadow:
+      3px 3px 0 #EF26F3,
+      -1px -1px 0 #EF26F3,
+      1px -1px 0 #EF26F3,
+      -1px 1px 0 #EF26F3,
+      1px 1px 0 #EF26F3;
+
+  }
 </style>
 
