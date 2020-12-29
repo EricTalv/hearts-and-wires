35facafffa5758bc5927fd2b4f2bb8a089493f0e
EricTalv <eric.talviste@tptlive.ee>
2020-12-29 17:20:43 +0200

    media queries

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,20 @@
 <template>
-  <div class="container h-100">
+  <div class="container-fluid h-100">
 
     <div class="row h-100">
       <div class="col-md-12">
-        <div class="row h-100 align-items-center">
-          <div class="col-md-8 ">
-            <h3>
-              <h1 class="welcome-title">HEARTS & WIRES</h1>
-            </h3>
+        <div class="row h-100 align-items-center justify-content-center">
+          <div class=" d-flex justify-content-end">
+            <h1 class="welcome-title ">HEARTS & WIRES</h1>
           </div>
-          <div class="col-md-1 vr">
+          <div class="vr">
           </div>
-          <div class="col-md-3">
-            <div class="btn-group btn-group-vertical" role="group">
-              <div class="nav-buttons d-flex flex-column align-items-start">
-                <button>About</button>
-                <button>Music</button>
-                <button>Blog</button>
-                <button>Contact</button>
-              </div>
+          <div class="btn-group btn-group-vertical" role="group">
+            <div class="nav-buttons d-flex flex-column align-items-start">
+              <button>About</button>
+              <button>Music</button>
+              <button>Blog</button>
+              <button>Contact</button>
             </div>
           </div>
         </div>
