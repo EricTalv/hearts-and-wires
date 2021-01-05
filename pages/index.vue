d8fbfcc152d407288adfd7626fa14b22fbc6d2f0
erictalv <eric.talviste@tptlive.ee>
2021-01-05 15:44:03 +0200

    remaking the front page

--- pages/index.vue
+++ pages/index.vue
@@ -7,46 +7,32 @@
           <div class=" d-flex justify-content-end">
             <h1 class="welcome-title ">HEARTS & WIRES</h1>
           </div>
-          <div class="vr">
-          </div>
-          <div class="btn-group btn-group-vertical" role="group">
-            <div class="nav-buttons d-flex flex-column align-items-start">
-              <button>About</button>
-              <button>Music</button>
-              <button>Blog</button>
-              <button>Contact</button>
-            </div>
-          </div>
         </div>
+
       </div>
     </div>
   </div>
+  </div>
+
 </template>
 
 <script>
 
-  import Logo from '~/components/Logo.vue'
+import Logo from '~/components/Logo.vue'
 
-  export default {
-    components: {
-      Logo
-    },
-    head() {
-      return {
-        script: [{src: 'https://identity.netlify.com/v1/netlify-identity-widget.js'}],
-      };
-    },
-  }
+export default {
+  components: {
+    Logo
+  },
+  head() {
+    return {
+      script: [{src: 'https://identity.netlify.com/v1/netlify-identity-widget.js'}],
+    };
+  },
+}
 </script>
 
 <style>
-  .hw-title {
-    font-family: "Tulpen One", cursive;
-    font-size: 20rem;
-    color: transparent;
-    -webkit-text-stroke-width: 3px;
-    -webkit-text-stroke-color: #EF26F3;
 
-  }
 </style>
 
