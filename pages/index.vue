499d0cdafd350b3b82c67f2d0f7b17037ffaab5d
erictalv <eric.talviste@tptlive.ee>
2021-01-05 16:16:40 +0200

    adding media queries

--- pages/index.vue
+++ pages/index.vue
@@ -44,9 +44,30 @@ export default {
   .welcome-title {
     font-family: "Tulpen One", cursive;
     color: #EF26F3;
-    font-size: 5vw;
+    font-size: 20vw;
     font-weight: bold;
     margin: 0;
   }
+
+  .nav-buttons > button {
+    background-color: transparent;
+    font-family: "Tulpen One", cursive;
+    color: #EF26F3;
+    font-size: 10vw;
+    border: 0;
+    outline: none;
+    transition: all 0.5s;
+  }
+  .nav-buttons > button:hover {
+    color: #35d4bc;
+    margin: 0px 5px 0px 5px;
+  }
+
+  @media screen and (max-width: 500px) {
+    .nav-buttons > button {
+      color: red;
+    }
+  }
+
 </style>
 
