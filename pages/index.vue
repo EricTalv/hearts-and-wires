f196114d76f1c230d3cd19f8c0687732d1791094
erictalv <eric.talviste@tptlive.ee>
2021-01-06 21:51:03 +0200

    messing around with font sizing

--- pages/index.vue
+++ pages/index.vue
@@ -15,15 +15,18 @@
       </div>
     </div>
     <div class="row">
-      <h1>About Us</h1>
-      <div class="col-2">
-        <img src="https://picsum.photos/200" alt="">
+      <div class="col-12 ">
+        <h1>About Us</h1>
+        <div class="float-left">
+          <img src="https://picsum.photos/200/250" alt="">
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, ipsa. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Ad aspernatur, aut beatae cupiditate delectus dicta distinctio doloribus earum
+          eos expedita iste labore magni minima minus modi, necessitatibus obcaecati omnis porro quis quisquam quos
+          reiciendis repudiandae suscipit tempore, veniam! Adipisci, commodi dolore eligendi eos inventore laudantium
+          nostrum reiciendis repellendus ut velit.</p>
       </div>
-      <div class="col-10">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolore, doloribus eum excepturi fuga laboriosam odio praesentium quae tempore vero.
-        </p>
-      </div>
+
     </div>
   </div>
 
@@ -44,46 +47,60 @@ export default {
 }
 </script>
 
-<style>
+<style lang="css" scoped>
 
-  hr {
-    border-color: #6f0071;
-    width: 40%;
-  }
+h1, p {
+  font-family: "Tulpen One", cursive;
+  color: #EF26F3;
+}
 
-  .welcome-title {
-    font-family: "Tulpen One", cursive;
-    color: #EF26F3;
-    font-size: 20vw;
-    font-weight: bold;
-    margin: 0;
-    padding: 0;
+h1 {
+  font-size: 5vw;
+}
+
+p {
+  font-size: 2.5vw;
+}
+
+hr {
+  border-color: #6f0071;
+  width: 40%;
+}
+
+.welcome-title {
+  font-family: "Tulpen One", cursive;
+  color: #EF26F3;
+  font-size: 20vw;
+  font-weight: bold;
+  margin: 0;
+  padding: 0;
+}
+
+.nav-buttons > button {
+  background-color: transparent;
+  font-family: "Tulpen One", cursive;
+  color: #EF26F3;
+  font-size: 6vw;
+  border: 0;
+  outline: none;
+  transition: all 0.5s;
+}
+
+.nav-buttons > button:hover {
+  color: #35d4bc;
+  margin: 0px 5px 0px 5px;
+}
+
+@media screen and (max-width: 500px) {
+  .nav-buttons {
+    display: flex;
+    flex-flow: column;
   }
 
   .nav-buttons > button {
-    background-color: transparent;
-    font-family: "Tulpen One", cursive;
-    color: #EF26F3;
-    font-size: 6vw;
-    border: 0;
-    outline: none;
-    transition: all 0.5s;
+    font-size: 15vw;
   }
-  .nav-buttons > button:hover {
-    color: #35d4bc;
-    margin: 0px 5px 0px 5px;
-  }
-
-  @media screen and (max-width: 500px) {
-    .nav-buttons  {
-      display: flex;
-      flex-flow: column;
-    }
-
-    .nav-buttons > button {
-      font-size: 15vw;
-    }
-  }
+}
 
 </style>
 
